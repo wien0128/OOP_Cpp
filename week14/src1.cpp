2e8a9fa80b135092ffe556e53f705a32cbf23ac5
Hyunbin Kim <taque88@naver.com>
2024-06-07 14:47:31 +0900

    week14

--- week14/src1.cpp
+++ week14/src1.cpp
@@ -2,9 +2,74 @@
 
 using namespace std;
 
+
+template<class T>
+T* remove(T src[], int sizeSrc, T minus[], int sizeMinus, int& retSize)
+{
+    T* retArray = new T[sizeSrc];
+    retSize = 0;
+
+    for (int i = 0; i < sizeSrc; ++i)
+    {
+        bool flag{ false };
+        for (int j = 0; j < sizeMinus; ++j)
+        {
+            if (src[i] == minus[j])
+            {
+                flag = true;
+                break;
+            }
+        }
+        if(!flag)
+        {
+            retArray[retSize++] = src[i];
+        }
+    }
+
+    return retArray;
+}
+
 int main()
 {
+    // remove() 함수를 int로 구체화하는 경우
+    cout << "정수 배열 {1,2,3,4}에서 정수 배열 {-3,5,10,1,2,3}을 뺍니다" << endl;
+    int x[]={1,2,3,4};
+    int y[]={-3,5,10,1,2,3};
+    int retSize;
+    int* p = remove(x, 4, y, 6, retSize);
 
-    
+    if (retSize == 0) 
+    {
+        cout << "모두 제거되어 리턴하는 배열이 없습니다." << endl;
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i < retSize; ++i) // 배열의 모든 원소 출력
+            cout << p[i] << ' ';
+            cout << endl;
+            delete [] p; // 할당받은 배열 반환
+    }
+
+
+    // remove() 함수를 double로 구체화하는 경우
+    cout << "Double 배열 {1.1, 2.2, 3.3, 4.4}에서 정수 배열 {-3.3, 5.5, 10.1, 1.1, 2.2, 3.3}을 뺍니다" << endl;
+    double dx[]={ 1.1, 2.2, 3.3, 4.4 };
+    double dy[]={-3.3, 5.5, 10.1, 1.1, 2.2, 3.3};
+    double* dp = remove(dx, 4, dy, 6, retSize);
+
+    if (retSize == 0) 
+    {
+        cout << "모두 제거되어 리턴하는 배열이 없습니다." << endl;
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i < retSize; ++i) // 배열의 모든 원소 출력
+            cout << dp[i] << ' ';
+            cout << endl;
+            delete [] p; // 할당받은 배열 반환
+    }
+        
     return 0;
 }
--- week14/src2.cpp
+++ week14/src2.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    map<string, string> dic;
 
+    // 단어 3개를 map에 저장
+    dic.insert({"love", "사랑"}); // ("love", "사랑") 저장
+    dic.insert({"apple", "사과"}); // ("apple", "사과") 저장
+    dic["cherry"] = "체리"; // ("cherry", "체리") 저장
+
+    cout << "저장된 단어 개수 " << dic.size() << endl;
+
+    string eng;
+    while (true)
+    {
+        cout << "찾고 싶은 단어>> ";
+        cin >> eng;// 사용자로부터 키 입력
+
+        if (eng == "exit")
+            break; // "exit"이 입력되면 종료
+            
+        if(dic.count(eng) == 0) // eng '키'를 끝까지 찾았는데 없음
+            cout << "없음" << endl;
+        else
+            cout << dic[eng] << endl; // dic에서 eng의 값을 찾아 출력
+    }
+    cout << "종료합니다..." << endl;
     
     return 0;
 }
